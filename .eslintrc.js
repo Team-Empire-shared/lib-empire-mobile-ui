cdad1c7f8e1d9bf7f379330826a6fc6175fbfa7e
Nidin Nover <nidinnover@teamempire.org>
2026-04-07 01:05:20 +0530

    fix(ci): add ESLint config (was missing, lint step crashed)

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: { jsx: true },
+  },
+  env: { node: true, es2022: true },
+  rules: {
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-explicit-any": "warn",
+  },
+  ignorePatterns: ["dist/", "node_modules/", "*.js"],
+};
